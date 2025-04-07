dba7b2571aab226999df128425c194b0ca328981
maxximus-ayson <maxximusayson25@gmail.com>
2025-04-07 12:14:51 +0800

    Navbar

---
+++ app/Views/layout/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://fonts.googleapis.com/css2?family=Teachers&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <title>Chainlogic</title>
+  <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+</head>
+<body>
+  <nav>
+    <div class="logo">Logo</div>
+    <div class="nav-links">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
+    </div>
+  </nav>
---
+++ app/Views/pages/homepage.php
@@ -0,0 +1,31 @@
+<?php include(APPPATH . 'Views/layout/header.php'); ?>
+
+<!-- <div class="main">
+  <div class="main-text">
+    <h1>Lorem Ipsum dolor</h1>
+    <p>
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+    </p>
+  </div>
+  <div class="main-img">
+    <img src="https://i.imgur.com/5zOZ6WR.jpeg" alt="Modern Building">
+  </div>
+</div>
+
+<div class="section">
+  <img src="https://i.imgur.com/Bf6idD0.jpeg" alt="City Buildings">
+  <div class="section-text">
+    <h2>Who we are</h2>
+    <p>
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo.
+    </p>
+  </div>
+</div>
+
+</body>
+</html> -->
