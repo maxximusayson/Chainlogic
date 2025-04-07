8215b97664464053dcd753748d2c3a94af6db50d
maxximus-ayson <maxximusayson25@gmail.com>
2025-04-07 14:14:45 +0800

    navchanges

--- app/Views/pages/homepage.php
+++ app/Views/pages/homepage.php
@@ -1,16 +1,15 @@
 <?php include(APPPATH . 'Views/layout/header.php'); ?>
 
 <!-- Home Section -->
- <br><br><br> <br>
+<br><br>
 <section class="home-section">
   <div class="home-text">
-    <h1>Lorem Ipsum<br>dolor</h1>
-    <p>
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-      Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-    </p>
+  <h1>Innovating Your Business</h1>
+<p>
+  Chainlogic provides innovative solutions to streamline your operations and boost productivity. Our team offers cutting-edge technology and expert support to keep your business ahead of the curve.
+</p>
+
+
   </div>
   <div class="home-image">
     <img src="images/hm1.png" alt="Building Image">
@@ -21,7 +20,7 @@
   <!-- Who We Are Section -->
   <section class="who-we-are">
     <div class="who-image">
-      <img src="images/hm1.png" alt="City Image">
+      <img src="images/hm2.png" alt="City Image">
     </div>
     <div class="who-text">
       <h2>Who we are</h2>
